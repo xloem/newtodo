d0f1830e61539dd743cc723167ad34ce23023ef1
Karl Semich <0xloem@gmail.com>
2022-09-10 06:22:04 -0400

    upload.bash

--- upload.bash
+++ upload.bash
@@ -6,4 +6,8 @@ git -C "$LOCAL_MIRROR" config gc.auto 0
 git push --force "$LOCAL_MIRROR"
 git -C "$LOCAL_MIRROR" update-server-info
 
-HOME="$LOCAL_MIRROR" arkb deploy --auto-confirm --use-bundler=https://node2.bundlr.network "$LOCAL_MIRROR" #| tee arkb-log
+HOME="$LOCAL_MIRROR" arkb deploy --auto-confirm --use-bundler=https://node2.bundlr.network "$LOCAL_MIRROR" | tee arkb-log
+
+ARWEAVE_URL="$(sed -ne 's!.*\(https://arweave.net/[-_=a-zA-Z0-9]*\).*!\1!p' arkb-log)"
+
+git remote set-url arweave "$ARWEAVE_URL"
